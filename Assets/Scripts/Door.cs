6b2706c2ca69037b8a6577a55afffbb5fa7a6082
R1nge <r1ngegamedev@gmail.com>
2023-04-04 16:37:46 +0300

    feat: door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using Player;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+
+    private void Awake() => _animator = GetComponent<Animator>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerCharacter _))
+        {
+            _animator.SetBool(IsOpen, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerCharacter _))
+        {
+            _animator.SetBool(IsOpen, false);
+        }
+    }
+}
---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(LoadMap_c());
+    }
+
+    private IEnumerator LoadMap_c()
+    {
+        yield return Resources.UnloadUnusedAssets();
+        SceneManager.LoadScene(sceneName);
+    }
+}
